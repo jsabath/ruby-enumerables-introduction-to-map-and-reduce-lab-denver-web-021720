2a2dad09bc36abc0b80f1912a7e94fb21cd8ddda
Justin Sabath <justin.sabath@aol.com>
2020-01-16 17:47:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,10 @@
 # My Code here....
+  
+def map_to_negativize(array)  
   new = []
   i = 0
-  while i < source_array.length do
-    new.push( source_array[i] * -1 )
+  while i < array.length do
+    new.push(array[i] * -1 )
     i += 1
   end
   return new
