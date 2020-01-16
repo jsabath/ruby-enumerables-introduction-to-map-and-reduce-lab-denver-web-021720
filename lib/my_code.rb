3adffa32c12c6f8b59a829396f3277772f0b05ac
Justin Sabath <justin.sabath@aol.com>
2020-01-16 17:45:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,8 @@
 # My Code here....
   new = []
   i = 0
-  while i < source_array.length do
-    new.push( source_array[i] * -1 )
+  while i < array.length do
+    new.push(array[i] * -1 )
       i += 1
     end
   return new
