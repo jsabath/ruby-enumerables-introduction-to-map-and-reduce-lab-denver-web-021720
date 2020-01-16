4b87957e036e0172f9ab9256cbdd3480fd46cc81
Justin Sabath <justin.sabath@aol.com>
2020-01-16 17:49:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,4 +20,12 @@ def map_to_no_change(array)
   return new
 end
 
-
+def map_to_double(array)
+  new = []
+  i = 0
+  while i < array.length do
+    new.push(array[i] * 2 )
+    i += 1
+  end
+  return new
+end
